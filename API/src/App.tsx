39a6cc2f76445b62d73b9d6871f92b6a2408edb4
LaAmaDelSexo <andres.ojeda.inbox@gmail.com>
2026-05-25 14:38:55 -0400

    good enough

--- API/src/App.tsx
+++ API/src/App.tsx
@@ -7,6 +7,7 @@ import Nav from './common/components/index/nav'
 import Home from './common/components/index/home'
 import Hero from './common/components/index/hero'
 import Highlights from './common/components/index/highlights'
+import Footer from './common/components/index/footer'
 
 function App() {
   return (
@@ -15,14 +16,7 @@ function App() {
       <Hero />
       <Home />
       <Highlights />
-      <section id="Contact">
-        <footer>
-          <h2>Contactanos</h2>
-          <p>
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-          </p>
-        </footer>
-      </section>
+      <Footer />
     </>
   )
 }
