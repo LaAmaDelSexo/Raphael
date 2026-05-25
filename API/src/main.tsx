705e0ac500f02bae4bde7f800992fa0f3962ea80
LaAmaDelSexo <andres.ojeda.inbox@gmail.com>
2026-05-25 14:23:30 -0400

    fix & Refactor: Dom Working and Api fragmentation

--- API/src/main.tsx
+++ API/src/main.tsx
@@ -4,9 +4,15 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 //import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import './index.css'
 import App from './App.tsx'
+import Home from './common/components/index/home.tsx';
 
 const router =createBrowserRouter([
   {path:"/",element:<App />},
+  {path:"/Home",element:<Home />},
+//  {path:"/About",element:<About />},
+//  {path:"/Showcase",element:<Showcase />}
+//  {path:"/Registration",element:<Registration />},
+//  {path:"/Login",element:<Login />},
 ])
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
