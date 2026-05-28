91bfdbea8cc8a9c8b93fd8b6b57bcf1bfeb25414
LaAmaDelSexo <andres.ojeda.inbox@gmail.com>
2026-05-28 18:17:44 -0400

    close: Skelly AboutUs done.

--- API/src/common/components/about/Hero.tsx
+++ API/src/common/components/about/Hero.tsx
@@ -39,11 +39,24 @@ function Hero () {
           </div>
         </div>
       </div>
-      <div className="">
-        
-      </div>
-      <div>
+      <div className="founders-container">
+        <div className="portraits">
+          <div className="founder"></div>
+          <div className="founder"></div>
+          <div className="founder"></div>
+          <div className="founder"></div>
+        </div>
+        <div className="founder-info">
 
+        </div>
+      </div>
+      <div className="history-track">
+        <div className="history-track-container">
+          <p>
+            Section 1.10.32 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC
+            "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+          </p>
+        </div>
       </div>
     </section>
 
